0903eac5db6216d820c78eee09158eea438fc732
kalaiprakash93 <kalaiprakash93@gmail.com>
2022-04-06 15:29:05 +0530

    Update HWPHello.m

--- src/ios/HWPHello.m
+++ src/ios/HWPHello.m
@@ -15,4 +15,18 @@
     [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
 
+- (void) checkFileLockStatus: (CDVInvokedUrlCommand *) command {
+    NSError * error;
+    NSString *filePath = [command.arguments objectAtIndex:0];
+    NSDictionary *attributes = [[NSFileManager defaultManager] attributesOfItemAtPath:filePath error:&error];
+    BOOL isLocked = [[attributes objectForKey:NSFileImmutable] boolValue];
+                
+    if (isLocked) {
+        NSLog(@"File is locked");
+    }
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:isLocked];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    return;
+}
+
 @end
